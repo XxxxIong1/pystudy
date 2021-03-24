3e04623bdc81600be20cff37b43ed439d38a38d0
XxxxIong1 <1747952877@qq.com>
2021-03-24 13:42:26 +0800

    first git

---
+++ alien_1.py
@@ -0,0 +1,17 @@
+alien_0 = {'x_position':0,'y_position':25,'speed':'medium'}
+print("Original x-position:"+str(alien_0['x_position']))
+alien_0['speed'] = 'fast'
+
+#向右移动外星人
+#据外星人当前速度决定将其移动多远
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    #移动很快
+    x_increment = 3
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print("New x-position:"+str(alien_0['x_position']))
+
+print("first git")
